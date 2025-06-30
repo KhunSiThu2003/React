e2462bffb8843d25a693c3d19a7c24664f60afb0
KhunSiThu2003 <khunsithu350@gmail.com>
2025-07-01 00:42:45 +0630

    7-1-2025

--- voucher-app/src/App.jsx
+++ voucher-app/src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import 'flowbite';
+import React from 'react'
 
 const App = () => {
   return (
--- voucher-app/src/main.jsx
+++ voucher-app/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "flowbite";
+import { RouterProvider } from "react-router-dom";
+import router from "./router.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
